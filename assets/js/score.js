df67d7238e98271e0533932b3e2596eca8dddc47
Raj.Dhadda <Raj.Dhadda@UK-L-TRESC14385>
2024-03-01 09:58:46 +0000

    fix: selected a more appropriate naming convention

--- assets/js/score.js
+++ assets/js/score.js
@@ -1,7 +1,9 @@
 const finalScore = document.getElementById("final-score-number");
 const finalScoreText = document.getElementById("final-score-text");
+//variable to access the stored score
 const score = parseInt(localStorage.getItem("score")) || 0;
 
+// If statement to give a phrase depending on players outcome
 if (score <= 3) {
   finalScoreText.innerHTML = "Keep trying, you'll get better!";
   finalScore.innerText = `${score}`;
