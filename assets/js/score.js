89f6832b49f246b19ba20d3949e13a0ca55412e7
Raj.Dhadda <Raj.Dhadda@UK-L-TRESC14385>
2024-03-20 09:39:04 +0000

    feat: added name to local storage to be displayed on score.html

--- assets/js/score.js
+++ assets/js/score.js
@@ -2,15 +2,16 @@ const finalScore = document.getElementById("final-score-number");
 const finalScoreText = document.getElementById("final-score-text");
 //variable to access the stored score
 const score = parseInt(localStorage.getItem("score")) || 0;
+let name = localStorage.getItem("name");
 
 // If statement to give a phrase depending on players outcome
 if (score <= 3) {
-  finalScoreText.innerHTML = "Keep trying, you'll get better!";
+  finalScoreText.innerHTML = `Keep trying ${name}, you'll get better!`;
   finalScore.innerText = `${score}`;
 } else if (score <= 6) {
-  finalScoreText.innerHTML = "Good effort! Keep practicing";
+  finalScoreText.innerHTML = `Good effort ${name}! Keep practicing`;
   finalScore.innerText = `${score}`;
 } else if (score === 10) {
-  finalScoreText.innerHTML = "Perfect score! Well done!";
+  finalScoreText.innerHTML = `Perfect score ${name}! Well done!`;
   finalScore.innerText = `${score}`;
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,14 @@
 // script for how to play accordion
 var accordion = document.querySelector(".accordion");
+const submit = document.getElementById("submit");
 
 accordion.addEventListener("click", function () {
   var panel = this.nextElementSibling;
-
   panel.classList.toggle("expanded");
+});
+
+submit.addEventListener("click", () => {
+  var nameInput = document.getElementById("name").value;
+  localStorage.setItem("name", nameInput);
+  console.log(nameInput);
 });
