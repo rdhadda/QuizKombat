662276c7fa4d4d32ba4074fb087143fd99ebbb90
Raj.Dhadda <Raj.Dhadda@UK-L-TRESC14385>
2024-04-05 11:23:35 +0100

    fix: renamed name variable to playerName

--- assets/js/score.js
+++ assets/js/score.js
@@ -1,17 +1,17 @@
 const finalScore = document.getElementById("final-score-number");
 const finalScoreText = document.getElementById("final-score-text");
-//variable to access the stored score
+//variable to access the stored score and player's name
 const score = parseInt(localStorage.getItem("score")) || 0;
-let name = localStorage.getItem("name");
+let playerName = localStorage.getItem("name");
 
-// If statement to give a phrase depending on players outcome
+// If statement to give a phrase depending on player's outcome
 if (score <= 3) {
-  finalScoreText.innerHTML = `Keep trying ${name}, you'll get better!`;
+  finalScoreText.innerHTML = `Keep trying ${playerName}, you'll get better!`;
   finalScore.innerText = `${score}`;
 } else if (score <= 6) {
-  finalScoreText.innerHTML = `Good effort ${name}! Keep practicing`;
+  finalScoreText.innerHTML = `Good effort ${playerName}! Keep practicing`;
   finalScore.innerText = `${score}`;
 } else if (score === 10) {
-  finalScoreText.innerHTML = `Perfect score ${name}! Well done!`;
+  finalScoreText.innerHTML = `Perfect score ${playerName}! Well done!`;
   finalScore.innerText = `${score}`;
 }
